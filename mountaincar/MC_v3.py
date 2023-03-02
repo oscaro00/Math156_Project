cdabaa6fd2e86d74cbeef61ed6ea4042b7c08923
Shan Jiang <shanjiang7@g.ucla.edu>
2023-03-02 11:13:04 -0800

    original function (change decay and learning rate)
    In addition, I found increase batch size does not perform well.

--- mountaincar/MC_v3.py
+++ mountaincar/MC_v3.py
@@ -41,10 +41,10 @@ np.random.seed(458)
 param_dict = {
     'epsilon': 1.0,
     'epsilon_min': .01,
-    'epsilon_decay': .9995, # try .9999 or .997
+    'epsilon_decay': .9995,# try .9999 or .997
     'gamma': .95,
     'batch_size': 64, # try 128
-    'lr': .001, # try .002
+    'lr': .002, # try .002
     'memory': 100000,
     'max_steps': 600 # try 1000
 }
